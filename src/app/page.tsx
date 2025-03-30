d2b8410ae5361c98f69b3ef85ba1898799807054
Jakub Kowieski <kubakowieski5151@gmail.com>
2025-03-30 07:41:37 +0200

    ostrzol Wstawaj2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="w-screen h-screen relative">
+            <div className="absolute top-0 w-screen h-8 bg-background z-99"></div>
             <Image
                 src="/kowies_app.jpg"
                 alt="Hero Image"
